9a6f806a47666673ca90ac84828e70b8201e99be
Salvatore <salvatore@rezo.net>
2020-02-05 10:15:51 +0100

    [Salvatore] [source:lang/ paquet-breves] Export depuis https://trad.spip.net de la langue fr [Salvatore] [source:lang/ paquet-breves] Mise a jour du bilan depuis https://trad.spip.net

--- lang/paquet-breves_fr.php
+++ lang/paquet-breves_fr.php
@@ -1,6 +1,6 @@
 <?php
 // This is a SPIP language file  --  Ceci est un fichier langue de SPIP
-// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/breves/lang
+// Fichier source, a modifier dans https://git.spip.net/spip/breves.git
 if (!defined('_ECRIRE_INC_VERSION')) {
 	return;
 }
