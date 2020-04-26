bd9a9d467a8da01522fcb3348b69d86f3cadb8ab
Bastian <s.donato@free.fr>
2020-04-26 20:45:04 +0200

    [Salvatore] [source:lang/ breves] Export depuis https://trad.spip.net de la langue oc_ni [Salvatore] [source:lang/ breves] Mise a jour du bilan depuis https://trad.spip.net

--- lang/breves_oc_ni.php
+++ lang/breves_oc_ni.php
@@ -10,10 +10,11 @@ if (!defined('_ECRIRE_INC_VERSION')) {
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
 	// B
+	'breve' => 'Brèva',
 	'breves' => 'Brèvas',
 
 	// E
-	'entree_breve_publiee' => 'Aquela brèva deu èstre publicada ?', # MODIF
+	'entree_breve_publiee' => 'Aquela brèva es:',
 	'entree_texte_breve' => 'Tèxt de la brèva',
 
 	// I
@@ -22,18 +23,20 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'icone_modifier_breve' => 'Modificar aquela brèva',
 	'icone_nouvelle_breve' => 'Escriure una novèla brèva',
 	'info_1_breve' => '1 brèva',
+	'info_aucun_breve' => 'Minga brèva',
 	'info_breves' => 'Lo voastre sit utilisa lo sistema de brèvas ?',
 	'info_breves_02' => 'Brèvas',
 	'info_breves_valider' => 'Brèvas de validar',
-	'info_gauche_numero_breve' => 'BRÈVA NÚMERO', # MODIF
-	'item_breve_proposee' => 'Brèva prepauada', # MODIF
-	'item_breve_refusee' => 'NON - Brèva refusada', # MODIF
-	'item_breve_validee' => 'AÏ- Brèva validada', # MODIF
+	'info_gauche_numero_breve' => 'Brèva nùmero',
+	'info_nb_breves' => '@nb@ brèvas',
+	'item_breve_proposee' => 'Prepauada à la valutacion',
+	'item_breve_refusee' => 'refudada',
+	'item_breve_validee' => 'validada',
 	'item_non_utiliser_breves' => 'Utilisar pas li brèvas',
 	'item_utiliser_breves' => 'Utilisar li brèvas',
 
 	// L
-	'logo_breve' => 'LÒGO DE LA BRÈVA', # MODIF
+	'logo_breve' => 'Lògo de la brèva',
 
 	// T
 	'texte_breves' => 'Li brèvas son de tèxtes corts e simples que permeton de metre en linha lèu d’informacions concisi, de gerar
@@ -42,6 +45,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	'titre_breve_publiee' => 'Breva publicada',
 	'titre_breve_refusee' => 'Brèva refudada',
 	'titre_breves' => 'Li brèvas',
-	'titre_langue_breve' => 'LENGA DE LA BRÈVA', # MODIF
+	'titre_langue_breve' => 'Lenga de la brèva',
+	'titre_nouvelle_breve' => 'Novèla brèva',
 	'titre_page_breves' => 'Brèvas'
 );
