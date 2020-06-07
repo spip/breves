4a355ed5df8421fc1178488d65c5da78558b02ad
nbdeditor <nbdeditor@gmail.com>
2020-06-07 21:20:02 +0200

    [Salvatore] [source:lang/ paquet-breves] Export depuis https://trad.spip.net de la langue bn [Salvatore] [source:lang/ paquet-breves] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/paquet-breves_bn.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-breves?lang_cible=bn
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'breves_description' => 'সংবাদ হচ্ছে সংক্ষিপ্ত তথ্য, কোন লেখক ছাড়া।',
+	'breves_slogan' => 'SPIP এ সংবাদের ব্যাবস্থাপনা'
+);
